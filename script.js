b826e3db8217b00b9683b3acf05fa79e3483e647
Mateus <mateusoliveira469@gmail.com>
2022-02-06 14:45:52 -0400

    random-pairs

--- script.js
+++ script.js
@@ -19,30 +19,34 @@ let cards = [];
 
 function insertImagesOnCards (cardFront) {
     for (let j = 0; j < arrayLenght/2; j++) {
+                
+        function compare() {
+            return Math.random() - 0.5;
+        }
+        
+        imgs.sort(compare);
+        
         imgs.push(cardFront[j], cardFront[j]);
-       /*  imgs.push(imgs[j]); */
-        /* console.log("test"); */
     }
 }
 
 
-/*for (let j = 0; j < arrayLenght/2; j++) {
-    cards.push(imgs[j], imgs[j]);
-    /* console.log("test"); 
-}*/
 
-function insertCardsOnScreen (toBeInserted) {
+
+
+function insertCardsOnScreen (toBeInserted, compare) {
     for (let i = 0; i < arrayLenght; i++) {
+
         const modify = document.querySelector(".cards-section");
         modify.innerHTML += `
         
         <div class="card">
             <div class="back-face face">
-            <img src="./img/${imgs[i]}.gif" alt="card-front" height=100 whidth=100>
+                <img src="./img/${imgs[i]}.gif" alt="card-front" height=100 whidth=100>
             </div>
             <div class="front-face face">
                 <img src="./img/front.png" alt="card-back" height=100 whidth=100>
-             </div>
+            </div>
         </div>
 
         ` ;
